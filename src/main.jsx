bfa6e840b6baeebd0a7b829a6c0709fccd62828d
Jhonatan Sousa <jhonatan.araujo1@hotmail.com>
2026-05-26 20:48:48 +0100

    feat(routing): add react-router-dom, /landing as proper React route

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,13 @@
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
+import Landing from './views/Landing.jsx'
 
-createRoot(document.getElementById('root')).render(<App />)
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/landing" element={<Landing />} />
+      <Route path="/*"       element={<App />} />
+    </Routes>
+  </BrowserRouter>
+)
